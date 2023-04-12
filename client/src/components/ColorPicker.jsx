de5d2068b8b7d151ed38a2fd5ea72f65c4284b46
Abhinav Jha <abhinavrocx@gmail.com>
2023-04-12 15:57:51 +0530

    feat: color picker

--- client/src/components/ColorPicker.jsx
+++ client/src/components/ColorPicker.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+// React color package
+import { SketchPicker } from 'react-color';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
 
 const ColorPicker = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>ColorPicker</div>
+    <div className="absolute left-full ml-3">
+      {/* view color selector */}
+      <SketchPicker
+        color={snap.color}
+        disableAlpha            //opacity
+        onChange={(color) => state.color = color.hex}
+      />
+    </div>
   )
-}
+};
 
 export default ColorPicker
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
@@ -14,6 +14,40 @@ const Customizer = () => {
     // get access to the state, check whether we are homepage or in Customizer
     const snap = useSnapshot(state);
 
+    const [file, setFile] = useState('');
+
+    const [prompt, setPrompt] = useState('');
+    const [generatingImg, setGeneratingImg] = useState(false);
+
+    const [activeEditorTab, setActiveEditorTab] = useState("");
+    const [activeFilterTab, setActiveFilterTab] = useState({
+        logoShirt: true,
+        stylishShirt: false,
+    })
+
+    // show tab content depending on the activeTab
+    const generateTabContent = () => {
+        switch (activeEditorTab) {
+            case "colorpicker":
+                return <ColorPicker />
+            case "filepicker":
+                return <FilePicker
+                    file={file}
+                    setFile={setFile}
+                    readFile={readFile}
+                />
+            case "aipicker":
+                return <AIPicker
+                    prompt={prompt}
+                    setPrompt={setPrompt}
+                    generatingImg={generatingImg}
+                    handleSubmit={handleSubmit}
+                />
+            default:
+                return null;
+        }
+    }
+
     return (
         <AnimatePresence>
             {/* If currently at intro page render home */}
@@ -24,8 +58,13 @@ const Customizer = () => {
                         <div className="flex items-center min-h-screen">
                             <div className="editortabs-container tabs">
                                 {EditorTabs.map((tab) => (
-                                    <Tab key={tab.name} tab={tab} handleClick={() => { }} />
+                                    <Tab
+                                        key={tab.name}
+                                        tab={tab}
+                                        handleClick={() =>     setActiveEditorTab(tab.name)} />
                                 ))}
+
+                                {generateTabContent()}
                             </div>
                         </div>
                     </motion.div>
