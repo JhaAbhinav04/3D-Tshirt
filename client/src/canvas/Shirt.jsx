6e8fff3a31d8b5349ff0a4b73a6b3f9d00bbce0a
Abhinav Jha <abhinavrocx@gmail.com>
2023-04-12 15:36:51 +0530

    feat: Camera movement

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { easing } from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame } from '@react-three/fiber';
+import { Decal, useGLTF, useTexture } from '@react-three/drei';
+
+import state from '../store';
+
+const Shirt = () => {
+    const snap = useSnapshot(state);
+    const { nodes, materials } = useGLTF('/shirt_baked.glb');
+
+    const logoTexture = useTexture(snap.logoDecal);
+    const fullTexture = useTexture(snap.fullDecal);
+    
+    // apply color smoothly
+    useFrame((state, delta) => easing.dampC(materials.lambert1.color, snap.color, 0.25, delta));
+
+    // current state string, used to check if state has changed. React will render model when state changes
+    const stateString = JSON.stringify(snap);
+
+    return (
+        <group key={stateString}>
+            <mesh
+                castShadow
+                geometry={nodes.T_Shirt_male.geometry}
+                material={materials.lambert1}
+                material-roughness={1}
+                dispose={null}
+            >
+                {/* showing logo or full texture */}
+                {snap.isFullTexture && (
+                    <Decal
+                        position={[0, 0, 0]}
+                        rotation={[0, 0, 0]}
+                        scale={1}
+                        map={fullTexture}
+                    />
+                )}
+
+                {snap.isLogoTexture && (
+                    <Decal
+                        position={[0, 0.04, 0.15]}
+                        rotation={[0, 0, 0]}
+                        scale={0.15}
+                        map={logoTexture}
+                        // change texture quality
+                        map-anisotropy={16}
+                        // rendering above others
+                        depthTest={false}
+                        depthWrite={true}
+                    />
+                )}
+            </mesh>
+        </group>
+    )
+};
+
+export default Shirt
