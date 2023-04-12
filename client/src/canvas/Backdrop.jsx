ab2e685e7406d313a3593b47b1c36034a5d550e0
Abhinav Jha <abhinavrocx@gmail.com>
2023-04-12 15:44:43 +0530

    feat: Bacground shadows

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { easing } from 'maath'
+import { useFrame } from '@react-three/fiber'
+// Shadows and light
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
+  const shadows = useRef();
+
   return (
-    <div>Backdrop</div>
+    <AccumulativeShadows
+      ref={shadows}
+      temporal          //smooth out shadow edges
+      frames={60}       //render at 60fps
+      alphaTest={0.85}    //transparency
+      scae={10}
+      rotation={[Math.PI / 2, 0, 0]}
+      position={[0, 0, -0.14]}
+    >
+      {/* all values by trial and error */}
+      <RandomizedLight
+        amount={4}
+        radius={9}
+        intensity={0.55}
+        ambient={0.25}
+        position={[5, 5, -10]}
+      />
+      <RandomizedLight
+        amount={4}
+        radius={5}
+        intensity={0.25}
+        ambient={0.55}
+        position={[-5, 5, -9]}
+      />
+    </AccumulativeShadows>
   )
-}
+};
 
 export default Backdrop
